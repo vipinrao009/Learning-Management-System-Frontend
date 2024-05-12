04c1935e5c0c3c703cbafd128dd6ce046a55f402
vipinrao009 <vipinrao2018@gmail.com>
2024-05-13 00:28:30 +0530

    Adding backend link

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/v1"
+const BASE_URL = "https://deployment-6zkz.onrender.com/api/v1"
 
 const axiosInstance = axios.create();
 
