09a57846cc944347a4ada1cd56c91514d0477816
vipinrao009 <vipinrao2018@gmail.com>
2025-05-13 14:44:28 +0530

    Update BASE_URL to production endpoint

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/v1"
+const BASE_URL = "https://deployment-5-bjnj.onrender.com/api/v1"
 
 const axiosInstance = axios.create();
 
